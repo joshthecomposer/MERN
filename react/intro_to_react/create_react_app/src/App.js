453f8705323515e48a8963bfdf0b27f54021c1ab
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-04-04 22:09:22 -0400

    --

--- react/intro_to_react/create_react_app/src/App.js
+++ react/intro_to_react/create_react_app/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="/meme" method="post">
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" className="form-control" />
+          <button className="btn btn-primary">Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
