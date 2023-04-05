7bfb8e573ff381224a4f89204bff8e81b0baafd7
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-04-04 22:40:43 -0400

    --

---
+++ react/functional_components/component_example.js
@@ -0,0 +1,11 @@
+import React from 'react';
+    
+const SomeFunctionalComponent = (props) => {
+    return (
+        <div>
+            This is our first functional component.
+        </div>
+    )
+}
+
+export default SomeFunctionalComponent;
---
+++ react/functional_components/functional_components_example_project/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import Person from './components/Person';
+import Product from "./components/Product";
+
+function App() {
+  return (
+    <div className="App">
+      <Person
+        firstName={"Josh"}
+        lastName={"Wise"}
+        age={32}
+        hairColor={"Brown"}
+      />
+      <Person
+        firstName={"Andrea"}
+        lastName={"Steere"}
+        age={32}
+        hairColor={"Black"}
+      />
+      <Person
+        firstName={"Brian"}
+        lastName={"Denmark"}
+        age={31}
+        hairColor={"Reddish"}
+      />
+      <Product title={"Dehydrated Water"} desc={"Just add 2 cups of water for a refreshing drink."} cost={ 49.98 } />
+      <Product title={"Watermelon"} desc={"Juicy sweet yum yum"} cost={29.98} />
+      <Product title={"Ipad"} desc={"A very expensive ipad from steve job"} cost={100} />
+    </div>
+  );
+}
+
+export default App;
